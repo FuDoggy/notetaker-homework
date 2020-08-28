5bd58274e10431359221b39bb27e0e950b1225ce
Kevin Fujimoto <fujimoto.kevin@gmail.com>
2020-08-27 20:38:11 -0700

    set up server.js and html.js

---
+++ Develop/routes/html.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  module.exports = app;
