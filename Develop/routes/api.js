9eb1b62f3be3521300e4002e1198887105c05b8b
Kevin Fujimoto <fujimoto.kevin@gmail.com>
2020-09-01 21:29:31 -0700

    finished post and delete routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -8,20 +8,7 @@ module.exports = function(app) {
         res.json(db)
     });
 
-    // app.post("/api/notes", function(req, res){
-    //     fs.readFile(__dirname + "/db/db.json", "utf8", function(err) {
-    //         if(err) throw err
-    //         // console.log(data)
-    //         console.log(req.body)
-    //         const userInput = JSON.parse(data)
-    //         req.body.id = userInput.length + 1
-    //         userInput.push(req.body)
-    //         console.log(userInput)
-    //     fs.writeFile(__dirname + "/db/db.json", JSON.stringify(userInput), function(err) {
-    //         if (err) throw err
-    //         })
-    //     })
-    // });
+
     app.post(`/api/notes`, function(req, res) {
         console.log(req.body)
         db.push(req.body); 
@@ -39,13 +26,24 @@ module.exports = function(app) {
         })
     });
 
-    app.delete(`/api/notes/:id`, function(req, res) {
-        console.log(req.params.id);
-        db.splice(req.params.id, 1);
-        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db), function(err) {
-            if (err) throw err;
-            res.send("Success")
-        })
-        //for loop to compare 
-    });
+    app.delete("/api/notes/:id", function(req, res){
+        const {id} = req.params;
+        console.log(id)
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
+            if(err) throw err
+            const dataArr = JSON.parse(data)
+            for (let i = 0; i < dataArr.length; i++) {
+               if(dataArr[i].id == id) {
+                   dataArr.splice(i, 1)
+               }
+            }
+            console.log(dataArr)
+
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(dataArr), function(err) {
+                if (err) throw err
+                res.send("Success")
+            })
+        })        
+    });  
+    
 }
